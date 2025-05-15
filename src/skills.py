e53c268d2ca36f649791b2f44424dd044d3a5380
Nassim Zaari <75166257+nassim1014@users.noreply.github.com>
2025-05-15 10:10:14 +0200

    Update skills.py

--- src/skills.py
+++ src/skills.py
@@ -20,9 +20,9 @@ def display_skills():
     with st.container():
         txt3('Programming Languages', '`Python`, `Java`, `SQL`, `HTML/CSS`')
         txt3('Data Science & ML', '`NumPy`, `Pandas`, `Scikit-learn`, `TensorFlow`, `PyTorch`, `Keras`')
-        txt3('Web Development', '`Django`, `Flask`, `Spring Boot`, `React`, `Streamlit`')
+        txt3('Web Development', '`Django`, `Flask`, `FastAPI`, `React`, `Streamlit`')
         txt3('Databases', '`MySQL`, `PostgreSQL`, `MongoDB`, `SQLite`')
         txt3('MLOps', '`Git`, `Docker`, `Mlflow`, `Linux`, `ZenML`')
         txt3('Data Visualization', '`Power BI`,`Matplotlib`, `Seaborn`, `Plotly`')
-        txt3('Big Data', '`Apache Spark`, `Hadoop`, `Kafka`')
+        txt3('Big Data', '`PySpark`, `Hadoop`, `Kafka`')
         txt3('Project Management', '`Agile`, `Scrum`, `Jira`, `ClickUp`')
